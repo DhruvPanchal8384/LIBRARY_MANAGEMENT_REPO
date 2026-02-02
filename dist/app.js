e72942aff99c87f62d76937a82b6142b8a5d58af
Dhruv <panchaldhruv8384@gmail.com>
2026-02-02 16:13:49 +0530

    added .js

--- dist/app.js
+++ dist/app.js
@@ -1,11 +1,13 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 require("reflect-metadata");
-const datasource_1 = require("../datasource");
+const datasource_1 = require("../datasource.js");
 const main_routes_1 = __importDefault(require("./routers/main.routes"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const morgan_1 = __importDefault(require("morgan"));
@@ -15,15 +17,19 @@ const app = (0, express_1.default)();
 const port = 1501;
 app.use(express_1.default.json());
 app.use((0, morgan_1.default)("dev"));
-app.use((0, cors_1.default)({
+app.use(
+  (0, cors_1.default)({
     origin: "*",
-}));
+  }),
+);
 app.use("/", main_routes_1.default);
 datasource_1.AppDataSource.initialize()
-    .then(async () => {
+  .then(async () => {
     console.log("Database Connected:");
-})
-    .catch((err) => console.log(err));
+  })
+  .catch((err) => console.log(err));
 app.listen(port, () => {
-    return console.log(`Express server is listening at http://localhost:${port} 🚀`);
+  return console.log(
+    `Express server is listening at http://localhost:${port} 🚀`,
+  );
 });
